35545a2359211a9338b28fa61cd6af8b2b4f6e84
oluwadamilareayoade <ayoads73@uwosh.edu>
2026-03-02 18:15:19 -0500

    Added start scree
    Game start screen

---
+++ Assets/Scripts/UIScripts.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScripts : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Prototype1");
+    }
+
+
+}
